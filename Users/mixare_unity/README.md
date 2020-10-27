8d709536dbd03c7df0a8708b41f59b5d07c33d21
nara-kim <58216009+nara-kim@users.noreply.github.com>
2020-10-27 19:52:20 +0900

    AR_Map

---
+++ Users/mixare_unity/README.md
@@ -0,0 +1,16 @@
+# Mixare 와 Vuforia 오픈소스를 이용한 AR 지도 어플리케이션
+
+개발환경 : Android Studio, Unity3D<br>
+Open Library: Mixare<br>
+Database for AR : Vuforia<br>
+
+[Mixare](http://www.mixare.org/)
+
+[Vuforia](https://www.vuforia.com/)
+
+
+## 구현 영상
+
+[![예제](http://img.youtube.com/vi/Ps5obYMwXwA/0.jpg)](https://youtu.be/Ps5obYMwXwA?t=0s) 
+
+(클릭하면 유튜브 링크로 넘어갑니다.)
---
+++ Users/mixare_unity/app/src/main/java/org/mixare/MixareActivity.java
@@ -0,0 +1,21 @@
+package org.mixare;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.File;
+
+public class MixareActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
